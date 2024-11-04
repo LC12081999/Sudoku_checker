98cf70b65b392b32b1420030cf3e0f5b55041bfc
Loicc <loicchristenn@gmail.com>
2024-11-04 10:46:38 +0100

    second commit

--- producer_consumer.c
+++ producer_consumer.c
@@ -6,10 +6,15 @@
 
 #define NUMITEMS 8
 
-int buffer[NUMITEMS] = {-1};
+unsigned int buffer[NUMITEMS] = {0};
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t c = PTHREAD_COND_INITIALIZER;
 
+int count = 0;
+int in = 0;
+int out = 0;
+
+/*
 int index_of_item(int buffer[NUMITEMS]) {
     for (int i = 0; i < NUMITEMS; i++) {
         if (buffer[i] != -1) {
@@ -27,6 +32,7 @@ int index_of_not_item(int buffer[NUMITEMS]) {
     }
     return -1;
 }
+
 
 void *production(void *arg) {
     srand(time(NULL));
@@ -67,10 +73,54 @@ void *consumation(void *arg) {
 
     return NULL;
 }
+*/
+
+void *production(void *arg) {
+    srand(time(NULL));
+    unsigned int i = 0;
+
+    while(1) {
+        pthread_mutex_lock(&m);
+        while (count == NUMITEMS) {
+            pthread_cond_wait(&c, &m);
+        }
+        buffer[in] = i;
+        printf("(Produced %d)\n", i);
+        in = (in + 1) % NUMITEMS;
+        count++;
+        
+        i = (i + 1) % NUMITEMS;
+        
+        pthread_cond_signal(&c);
+        pthread_mutex_unlock(&m);
+
+        float random_time = (rand() % 901 + 100) / 1000.0;
+        unsigned int microseconds = (unsigned int)(random_time * 1000000);
+        usleep(microseconds);
+    }
+
+    return NULL;
+}
+
+void *consumation(void *arg) {
+    while(1) {
+        pthread_mutex_lock(&m);
+        while (count == 0) {
+            pthread_cond_wait(&c, &m);
+        }
+        unsigned int item = buffer[out];
+        printf("(Consumed %d)\n", item);
+        out = (out + 1) % NUMITEMS;
+        count--;
+        pthread_cond_signal(&c);
+        pthread_mutex_unlock(&m);
+    }
+
+    return NULL;
+}
 
 int main() {
     pthread_t producer, consumer;
-
 
     pthread_create(&producer, NULL, production, NULL);
     pthread_create(&consumer, NULL, consumation, NULL);
