f552b04299908e8b3cb535bfc16109f4c4814fbc
Loicc <loicchristenn@gmail.com>
2024-11-04 13:05:49 +0100

    3 commit

---
+++ search.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int sudoku[9][9] = {
+    {5, 3, 4, 6, 8, 7, 9, 1, 2},
+    {6, 7, 2, 1, 9, 5, 3, 4, 8},
+    {1, 9, 8, 3, 4, 2, 5, 6, 7},
+    {8, 5, 9, 7, 6, 1, 4, 2, 3},
+    {4, 2, 6, 8, 5, 3, 7, 9, 1},
+    {7, 1, 3, 9, 2, 4, 8, 5, 6},
+    {9, 6, 1, 5, 3, 7, 2, 8, 4},
+    {2, 8, 7, 4, 1, 9, 6, 3, 5},
+    {3, 4, 5, 2, 8, 6, 1, 7, 9}
+};
+
+int searched_number = -1;
+int found = 0;
+
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t c = PTHREAD_COND_INITIALIZER;
+
+void *search(void *arg) {
+    int column = (int)(arg);
+    for (int i = 0; i < 9; i++) {
+        if (found) {
+            return NULL;
+        }
+        if (sudoku[i][column] == searched_number) {
+            pthread_mutex_lock(&m);
+            found = 1;
+            pthread_mutex_unlock(&m);
+            return NULL;
+        }
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t threads[9];
+
+    for (int i = 0; i < 9; i++) {
+        pthread_create(&threads[i], NULL, search, i);
+        pthread_join(threads[i], NULL);
+    }
+
+    if (found) {
+        printf("Le nombre a été trouvé.");
+    } else {
+        printf("Le nombre n'a pas été trouvé.");
+    }
+
+    pthread_mutex_destroy(&m);
+    pthread_cond_destroy(&c);
+    
+    return 0;
+}
